f55032bd7cb169bf981303c9d5af10e37a583f1b
Alex Olson <alex.olson@gis-ag.com>
2018-10-20 14:43:35 +0200

    hook

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio');
 
 const parseCommand = function (message) {
   const $ = cheerio.load(message);
-  console.log({ $: $.html(), $a: $('at').html() });
+  console.log({ $: $.html(), $a: $.html('at') });
 };
 
 const getBearerToken = function (authUrl, appId, appPassword) {
