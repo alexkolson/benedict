9430c14039e6285f71ad0298c0980ad0dae8ab42
Alex Olson <alex.olson@gis-ag.com>
2018-10-20 14:40:59 +0200

    moar logs.

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio');
 
 const parseCommand = function (message) {
   const $ = cheerio.load(message);
-  console.log({ $: $.html() });
+  console.log({ $: $.html(), $a: $('a').html() });
 };
 
 const getBearerToken = function (authUrl, appId, appPassword) {
