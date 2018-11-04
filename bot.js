daedb439513288cfcf2377853d7a957c71dab4bb
Alex Olson <alex.olson@gis-ag.com>
2018-11-04 20:34:18 +0100

    undo debugging stuff.

--- bot.js
+++ bot.js
@@ -241,7 +241,6 @@ const resetBenedict = function (hook) {
       return getAccessToken(hook);
     })
     .then(function (accessToken) {
-      console.log('getAccessToken');
       return request.post(url, {
         json: true,
         headers: {
